7404ddd7da2c4ab758dcef88eac577d9de92caa0
siddht4 <dhar.75.ap@gmail.com>
2020-06-03 16:06:21 +0530

    Update qr_gen.js

--- qr_gen.js
+++ qr_gen.js
@@ -5,11 +5,11 @@ _a[1]='pa='+urlParams['pa']
 _a[2]='pn='+urlParams['pn']
 _a[3]='mc=0000'
 _a[4]='tid='
-_a[5]='tr=SHD48856717875'
+_a[5]='tr='
 _a[6]='tn=Pay'
 _a[7]='am='+urlParams['amt']
 _a[8]='cu=INR'
-_a[9]='refUrl=https://business.paytm.com/payment-gateway'
+_a[9]='refUrl=https://zygolo.github.io/preview/main/'
 
 _b='',_s=''
 for (_b in _a)
