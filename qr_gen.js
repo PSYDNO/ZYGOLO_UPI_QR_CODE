495db3256248965ae2a6914870b20e3d2cf42bb0
siddht4 <15411956+siddht4@users.noreply.github.com>
2022-05-13 12:38:42 +0530

    Update qr_gen.js

--- qr_gen.js
+++ qr_gen.js
@@ -6,13 +6,13 @@ _a=[]
 _a[0]='upi://pay'
 _a[1]='pa='+urlParams['pa']
 _a[2]='pn='+urlParams['pn']
-_a[3]='mc=0000'
+_a[3]='mc=|
 _a[4]='tid='
 _a[5]='tr='
 _a[6]='tn='+urlParams['custm_msg']
 _a[7]='am='+result
 _a[8]='cu=INR'
-_a[9]='refUrl=https://zygolo.github.io/preview/main/'
+_a[9]='refUrl=BHIM_UPI'
 
 _b='',_s=''
 for (_b in _a)
