09ca2dfc6a7e4bc0fa5e651f6822f8d3ad804950
siddht4 <dhar.75.ap@gmail.com>
2023-06-26 09:11:54 +0530

    Update qr_gen.js
    modified a bit

--- qr_gen.js
+++ qr_gen.js
@@ -1,40 +1,29 @@
 var obj = new BigEval();
-result=obj.exec(urlParams['amt'])
-  
+var result = obj.exec(urlParams['amt']);
 
-_a=[]
-_a[0]='upi://pay'
-_a[1]='pa='+urlParams['pa']
-_a[2]='pn='+urlParams['pn']
-_a[3]='mc='
-_a[4]='tid='
-_a[5]='tr='
-_a[6]='tn='+urlParams['custm_msg']
-_a[7]='am='+result
-_a[8]='cu=INR'
-_a[9]='refUrl=BHIM_UPI'
+var params = {
+  pa: urlParams['pa'],
+  pn: urlParams['pn'],
+  mc: '',
+  tid: '',
+  tr: '',
+  tn: urlParams['custm_msg'],
+  am: result,
+  cu: 'INR',
+  refUrl: 'BHIM_UPI'
+};
 
-_b='',_s=''
-for (_b in _a)
-{
-	if(_b=='0')
-	{
-		_s+=_a[_b]+'?'
-	}
-	else
-	{
-		_s+='&'+_a[_b]
-	}
-}
-	
-new QRCode(document.getElementById("qrcode"), _s);
-console.log(qrcode)
-var a = document.createElement('a');  
-var link = document.createTextNode(" upi "); 
- 
-a.appendChild(link);
-  
-a.title = "UPI Link"; 
-  a.href = _s;  
+var paramString = Object.entries(params)
+  .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+  .join('&');
 
-          document.body.appendChild(a)    ;    
+var upiLink = `upi://pay?${paramString}`;
+
+new QRCode(document.getElementById('qrcode'), upiLink);
+
+var upiLinkEl = document.createElement('a');
+upiLinkEl.href = upiLink;
+upiLinkEl.title = 'UPI Link';
+upiLinkEl.innerText = 'upi';
+
+document.body.appendChild(upiLinkEl);
